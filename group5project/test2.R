55a7fe9c4cea776db9b7ee13662767bdc8497057
khanimzul <kbz5025@psu.edu>
2017-12-09 16:14:48 -0500

    rbind all datas for no tooth loss
    but data1 is lost

--- group5project/test2.R
+++ group5project/test2.R
@@ -24,13 +24,41 @@ data3=oral_health_service_2014_
 data4=oral_health_service_2015_
 data5=teeth_loss_6_crude_female_12_
 data6=teeth_loss_6_crude_male_12_
-data7=notoothlossMale12Age
 data8=AllTeethLostFem14_Age_
 data9=AllTeethLostMale14_Age_
 data10=crude_female_teeth_loss_6_more_14
 data11=crude_teeth_loss_6_more_male_14
+data12=NotoothlossBlack12_Age
+data13=NotoothlossBlack12_Crude
+data14=NotoothlossBlack14_Age
+data15=NotoothlossBlack14_Crude
+data16=NotoothlossFemale12_Age
+data17=NotoothlossFemale12_Crude
+data18=NotoothlossFemale14_Age
+data19=NotoothlossFemale14_Crude
+data20= NotoothlossMale12_Age
+data21=NotoothlossMale12_Crude
+data22=NotoothlossMale14_Age
+data23=NotoothlossMale14_Crude
+data24=NotoothlossMulti12_Age
+data25=NotoothlossMulti12_Crude
+data26=NotoothlossMulti14_Age
+data27=NotoothlossMulti14_Crude
+data28=NotoothlossOther12_Age
+data29=NotoothlossOther12_Crude
+data30=NotoothlossOther14_Age
+data31=NotoothlossOther14_Crude
+data32=NotoothlossWhite12_Age
+data33= NotoothlossWhite12_Crude
+data34= NotoothlossWhite14_Age
+data35= NotoothlossWhite14_Crude
+data36= NotoothlossHispanic12_Age
+data37= NotoothlossHispanic12_Age
+data38=NotoothlossHispanic12_Crude
+data39=NotoothlossHispanic14_Age
+data40=NotoothlossHispanic14_Crude
 
-data=rbind(data1,data2,data3,data4,data5,data6,data7,data8,data9,data10,data11)
+data=rbind(data1,data2,data3,data4,data5,data6,data8,data9,data10,data11,data12,data13,data14,data15,data16,data17,data18,data19,data20,data21,data22,data23,data24,data25,data26,data27,data28,data29,data30,data31,data32,data33,data34,data35,data36,data37,data38,data39,data40)
 
 ##tab
 ##visual tools: boxplot, pie chart, histogram
@@ -95,7 +123,7 @@ if (input$topicinterest != "--") {
     dataplot <- data[,7]
 
   }
-
+}
   count= table(data)
 
   # Render a barplot
